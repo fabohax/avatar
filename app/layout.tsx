c4f949b81f527e348e45a2c2b5396366737ddf8a
Fabo <40230@pm.me>
2024-03-21 18:07:21 -0500

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,48 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import siteMetadata from '@/data/siteMetadata'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Fabo Hax',
-  description: 'CG Architect & Software Developer from Brazil based in Callao, Perú',
+
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    default: siteMetadata.title,
+    template: `%s | ${siteMetadata.title}`,
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: './',
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: 'en_US',
+    type: 'website',
+  },
+  alternates: {
+    canonical: './',
+    types: {
+      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: siteMetadata.title,
+    card: 'summary_large_image',
+    images: [siteMetadata.socialBanner],
+  },
 }
 
 export default function RootLayout({
